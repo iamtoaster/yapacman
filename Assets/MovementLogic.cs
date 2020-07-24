5f03c196a811775e8910669e0e6e081957a077d2
Vladislav Slobodenyuk <toastergithubmail@gmail.com>
2020-07-24 15:21:55 +0900

    Refactor

---
+++ Assets/MovementLogic.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MovementLogic
+{
+    public float moveSpeed = 1f;
+    public Grid mapGrid;
+    public Transform movePoint;
+    public Vector3Int direction = new Vector3Int(1, 0, 0);
+    public Animator animPlayer;
+
+    public MovementLogic(float moveSpeed, Grid mapGrid, Transform movePoint, Vector3Int direction, Animator animPlayer)
+    {
+        this.moveSpeed = moveSpeed;
+        this.mapGrid = mapGrid ?? throw new ArgumentNullException(nameof(mapGrid));
+        this.movePoint = movePoint ?? throw new ArgumentNullException(nameof(movePoint));
+        this.direction = direction;
+        this.animPlayer = animPlayer ?? throw new ArgumentNullException(nameof(animPlayer));
+    }
+
+    public void ProcessMovement(GameObject target, float hInput, float vInput)
+    {
+        Vector3Int currCell = GetCurrentCell(target); // i don't know why but it works only like this
+
+        Tilemap tilemap = mapGrid.GetComponentInChildren<Tilemap>();
+
+        Vector3Int targetCell = currCell + direction;
+
+        if (Vector3.Distance(target.transform.position, movePoint.position) <= 0.5f)
+        {
+            if (Mathf.Abs(hInput) == 1f)
+            {
+                Vector3Int dir = new Vector3Int((int)hInput, 0, 0);
+                if (CheckCell(currCell + dir, tilemap))
+                {
+                    targetCell = currCell + dir;
+                    direction = dir;
+                }
+            }
+            else if (Mathf.Abs(vInput) == 1f)
+            {
+                Vector3Int dir = new Vector3Int(0, (int)vInput, 0);
+                if (CheckCell(currCell + dir, tilemap))
+                {
+                    targetCell = currCell + dir;
+                    direction = dir;
+                }
+            }
+        }
+
+        tilemap.SetTile(new Vector3Int(0, 0, 0), tilemap.GetTile(targetCell));
+
+        if (CheckCell(targetCell, tilemap))
+        {
+            movePoint.position = mapGrid.CellToWorld(targetCell) + (tilemap.cellSize / 2f);
+        }
+
+        // wrap around the screen
+        // the portal across the screen is at y = -17
+        if (currCell.x <= tilemap.cellBounds.xMin - 1)
+        {
+            target.transform.position = mapGrid.CellToWorld(new Vector3Int(tilemap.cellBounds.xMax - 1, -17, 0)) + (tilemap.cellSize / 2f);
+            targetCell = new Vector3Int(tilemap.cellBounds.xMax - 2, -17, 0);
+            direction = new Vector3Int(-1, 0, 0); // to the left
+
+        }
+        else if (currCell.x >= tilemap.cellBounds.xMax)
+        {
+            target.transform.position = mapGrid.CellToWorld(new Vector3Int(tilemap.cellBounds.xMin, -17, 0)) + (tilemap.cellSize / 2f);
+            targetCell = new Vector3Int(tilemap.cellBounds.xMin + 1, -17, 0);
+            direction = new Vector3Int(1, 0, 0); // to the right
+        }
+
+        // switch didn't work unfortunately
+        if (direction == new Vector3Int(1, 0, 0))
+        {
+            animPlayer.Play("Base Layer.Right");
+        }
+        if (direction == new Vector3Int(-1, 0, 0))
+        {
+            animPlayer.Play("Base Layer.Left");
+        }
+        if (direction == new Vector3Int(0, 1, 0))
+        {
+            animPlayer.Play("Base Layer.Up");
+        }
+        if (direction == new Vector3Int(0, -1, 0))
+        {
+            animPlayer.Play("Base Layer.Down");
+        }
+
+        target.transform.position = Vector3.MoveTowards(target.transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    }
+
+    public Vector3Int GetCurrentCell(GameObject target)
+    {
+        return mapGrid.WorldToCell(target.transform.position) + new Vector3Int(0, 1, 0);
+    }
+
+    public bool CheckCell(Vector3Int targetCell, Tilemap tilemap)
+    {
+        if (tilemap.GetTile(targetCell) != null)
+        {
+            if (tilemap.GetTile(targetCell).name != "Tile_coltiles_1") // if not wall
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return true; // if empty
+        }
+
+        return false; // if wall
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
@@ -11,102 +10,21 @@ public class Player : MonoBehaviour
     public Vector3Int direction = new Vector3Int(1, 0, 0);
 
     private Animator animPlayer;
+    private MovementLogic moveLogic;
+
 
     // Start is called before the first frame update
     void Start()
     {
         animPlayer = GetComponent<Animator>();
         movePoint.parent = null;
+
+        moveLogic = new MovementLogic(moveSpeed, mapGrid, movePoint, direction, animPlayer);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3Int currCell = mapGrid.WorldToCell(transform.position) + new Vector3Int(0, 1, 0);
-
-        var tilemap = mapGrid.GetComponentInChildren<Tilemap>();
-
-        Vector3Int targetCell = currCell + direction;
-
-        if (Vector3.Distance(transform.position, movePoint.position) <= 0.5f)
-        {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
-            {
-                Vector3Int dir = new Vector3Int((int)Input.GetAxisRaw("Horizontal"), 0, 0);
-                if (CheckCell(currCell + dir, tilemap))
-                {
-                    targetCell = currCell + dir;
-                    direction = dir;
-                }
-            }
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
-            {
-                Vector3Int dir = new Vector3Int(0, (int)Input.GetAxisRaw("Vertical"), 0);
-                if (CheckCell(currCell + dir, tilemap))
-                {
-                    targetCell = currCell + dir;
-                    direction = dir;
-                }
-            }
-        }
-
-        tilemap.SetTile(new Vector3Int(0, 0, 0), tilemap.GetTile(targetCell));
-
-        if (CheckCell(targetCell, tilemap))
-        {
-            movePoint.position = mapGrid.CellToWorld(targetCell) + new Vector3(0.25f, -0.25f, 0);
-        }
-
-        // wrap around the screen
-        if (currCell.x <= -1)
-        {
-            transform.position = mapGrid.CellToWorld(new Vector3Int(27, -17, 0)) + new Vector3(0.25f, -0.25f, 0);
-            targetCell = new Vector3Int(26, -17, 0);
-            direction = new Vector3Int(-1, 0, 0); // to the left
-
-        }
-        else if (currCell.x >= 28)
-        {
-            transform.position = mapGrid.CellToWorld(new Vector3Int(0, -17, 0)) + new Vector3(0.25f, -0.25f, 0);
-            targetCell = new Vector3Int(1, -17, 0);
-            direction = new Vector3Int(1, 0, 0); // to the right
-        }
-
-        // switch didn't work unfortunately
-        if (direction == new Vector3Int(1, 0, 0))
-        {
-            animPlayer.Play("Base Layer.Right");
-        }
-        if (direction == new Vector3Int(-1, 0, 0))
-        {
-            animPlayer.Play("Base Layer.Left");
-        }
-        if (direction == new Vector3Int(0, 1, 0))
-        {
-            animPlayer.Play("Base Layer.Up");
-        }
-        if (direction == new Vector3Int(0, -1, 0))
-        {
-            animPlayer.Play("Base Layer.Down");
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-    }
-
-    private bool CheckCell(Vector3Int targetCell, Tilemap tilemap)
-    {
-        if (tilemap.GetTile(targetCell) != null)
-        {
-            if (tilemap.GetTile(targetCell).name != "Tile_coltiles_1") // if not wall
-            {
-                return true;
-            }
-        }
-        else
-        {
-            return true; // if empty
-        }
-
-        return false; // if wall
+        moveLogic.ProcessMovement(this.gameObject, Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 }
