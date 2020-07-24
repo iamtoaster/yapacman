6a38a47c639b6d8425a4f7acf6608f6c455a990c
Vladislav Slobodenyuk <toastergithubmail@gmail.com>
2020-07-24 14:14:46 +0900

    Complete movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,16 +5,19 @@ using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 1f;
     public Grid mapGrid;
     public Transform movePoint;
     public Transform tarCell;
 
     public Vector3Int direction = new Vector3Int(1, 0, 0);
 
+    private Animator animPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
+        animPlayer = GetComponent<Animator>();
         tarCell.parent = null;
         movePoint.parent = null;
     }
@@ -24,35 +27,110 @@ public class Player : MonoBehaviour
     {
         Vector3Int currCell = mapGrid.WorldToCell(transform.position) + new Vector3Int(0,1,0);
 
-        Vector3Int targetCell = currCell + direction;
-        tarCell.position = mapGrid.CellToWorld(targetCell);
-
         var tilemap = mapGrid.GetComponentInChildren<Tilemap>();
 
-        tilemap.SetTile(new Vector3Int(0, 0, 0), tilemap.GetTile(targetCell));
-        if (tilemap.GetTile(targetCell) != null)
+        Vector3Int targetCell = currCell + direction;
+
+        Debug.Log(currCell);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.5f)
         {
-            if (tilemap.GetTile(targetCell).name != "Tile_coltiles_1")
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                movePoint.position = mapGrid.CellToWorld(targetCell) + new Vector3(0.25f, -0.25f, 0);
+                Vector3Int dir = new Vector3Int((int) Input.GetAxisRaw("Horizontal"), 0, 0);
+                if (CheckCell(currCell + dir, tilemap))
+                {
+                    targetCell = currCell + dir;
+                    direction = dir;
+                }
             }
-        } else
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                Vector3Int dir = new Vector3Int(0, (int)Input.GetAxisRaw("Vertical"), 0);
+                if (CheckCell(currCell + dir, tilemap))
+                {
+                    targetCell = currCell + dir;
+                    direction = dir;
+                }
+            }
+        }
+
+        tarCell.position = mapGrid.CellToWorld(targetCell);
+        tilemap.SetTile(new Vector3Int(0, 0, 0), tilemap.GetTile(targetCell));
+
+        if (CheckCell(targetCell, tilemap))
         {
             movePoint.position = mapGrid.CellToWorld(targetCell) + new Vector3(0.25f, -0.25f, 0);
         }
 
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-
-        /*if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        // wrap around the screen
+        if (currCell.y == -17)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            if (currCell.x <= -1)
             {
-                movePoint.position += Input.GetAxisRaw("Horizontal") * new Vector3(0.5f, 0f, 0f);
+                transform.position = mapGrid.CellToWorld(new Vector3Int(27, -17, 0)) + new Vector3(0.25f, -0.25f, 0);
+                targetCell = new Vector3Int(26, -17, 0);
+                direction = new Vector3Int(-1, 0, 0); // to the left
+
             }
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            else if (currCell.x >= 28)
             {
-                movePoint.position += Input.GetAxisRaw("Vertical") * new Vector3(0f, 0.5f, 0f);
+                transform.position = mapGrid.CellToWorld(new Vector3Int(0, -17, 0)) +new Vector3(0.25f, -0.25f, 0);
+                targetCell = new Vector3Int(1, -17, 0);
+                direction = new Vector3Int(1, 0, 0); // to the right
             }
-        }*/
+        }
+        else
+        {
+            if (currCell.x <= 0)
+            {
+                transform.position = mapGrid.CellToWorld(new Vector3Int(27, -17, 0)) + new Vector3(0.25f, -0.25f, 0);
+                targetCell = new Vector3Int(26, -17, 0);
+                direction = new Vector3Int(-1, 0, 0); // to the left
+            }
+            else if (currCell.x >= 27)
+            {
+                transform.position = mapGrid.CellToWorld(new Vector3Int(0, -17, 0)) + new Vector3(0.25f, -0.25f, 0);
+                targetCell = new Vector3Int(1, -17, 0);
+                direction = new Vector3Int(1, 0, 0); // to the right
+            }
+        }
+
+        // switch didn't work unfortunately
+        if (direction == new Vector3Int(1,0,0))
+        {
+            animPlayer.Play("Base Layer.Right");
+        }
+        if (direction == new Vector3Int(-1, 0, 0))
+        {
+            animPlayer.Play("Base Layer.Left");
+        }
+        if (direction == new Vector3Int(0, 1, 0))
+        {
+            animPlayer.Play("Base Layer.Up");
+        }
+        if (direction == new Vector3Int(0, -1, 0))
+        {
+            animPlayer.Play("Base Layer.Down");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    }
+
+    private bool CheckCell(Vector3Int targetCell, Tilemap tilemap)
+    {
+        if (tilemap.GetTile(targetCell) != null)
+        {
+            if (tilemap.GetTile(targetCell).name != "Tile_coltiles_1") // if not wall
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return true; // if empty
+        }
+
+        return false; // if wall
     }
 }
